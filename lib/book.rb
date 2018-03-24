ded7a8368fefd3c47c9d98259484753e1ceb65db
Kitty Davies <kittyfdavies@gmail.com>
2018-03-24 20:02:23 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,19 @@
 class Book
-  attr_accessor :author, :page_count,
-  attr_reader :title, :genre #since this is the reader for genre...
-
+  attr_accessor :author, :page_count 
+  attr_reader :title, :genre  #since this is the reader for genre...
+ 
   GENRES = [] #(add the empty constant array)
-
+ 
   def initialize(title)
     @title = title
   end
-
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+ 
   def genre=(genre) #... create the writer here and add the constant shovel code
     @genre = genre
-    GENRES << genre
+    GENRES << genre 
   end
 end
